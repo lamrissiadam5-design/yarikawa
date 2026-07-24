550162d11360b5f14bc8e516554f235d47a8a1b3
lamrissiadam5-design <lamrissiadam5@gmail.com>
2026-07-24 21:21:55 +0100

    Update bot activities status message

--- src/config/bot.js
+++ src/config/bot.js
@@ -21,10 +21,10 @@ export const botConfig = {
     // 3 = Watching
     // 4 = Custom
     // 5 = Competing
-    activities: [
+    activities: [3
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "am the evil l9!!",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
